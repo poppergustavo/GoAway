9ddb7a7f27d3267444209df7fec7703a04fcfa2f
Gustavo Pöpper <gustavo.popper@philips.com>
2022-08-24 21:57:36 -0300

    Build 0.3

--- GoAway-back/Hub-scripts.js
+++ GoAway-back/Hub-scripts.js
@@ -6,10 +6,3 @@ const side_menu_dots = document.getElementById('side_menu_dots_div');
 
 
 //Show content
-side_menu_dots.onclick = function(){
-
-    side_menu_content_p.style.display = "block;";
-    side_menu_content_p.style.transition = "1s;";
-
-
-};
