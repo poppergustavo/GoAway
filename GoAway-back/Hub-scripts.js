a26d2e9ad1463742c989632da08b74793347123c
Gustavo Pöpper <gustavo.popper@philips.com>
2022-10-17 20:39:16 -0300

    Dev commit 17 Oct

--- GoAway-back/Hub-scripts.js
+++ GoAway-back/Hub-scripts.js
@@ -3,6 +3,13 @@
 const side_menu_content_p = document.getElementById('side_menu_content');
 const side_menu_m = document.getElementById('side_menu');
 const side_menu_dots = document.getElementById('side_menu_dots_div');
-
+const plan_btn = document.getElementById('plan_btn');
+const planned_btn = document.getElementById('planned_btn');
+const discover_btn = document.getElementById('discover_btn');
+const options_btn = document.getElementById('options_btn');
 
 //Show content
+
+
+
+console.log('haha');
